b25b6ec012cc3132f1e6b160b6afe4c1e64f24c9
Aaris-Kazi <aariskazi@gmail.com>
2020-12-14 15:04:36 +0530

    Locating the faces

--- multi_images.py
+++ multi_images.py
@@ -1,12 +1,15 @@
 from PIL import Image
+import numpy as np
 import face_recognition
 
 #load the image
 images = face_recognition.load_image_file('./faces/donald trump.jpg')
 #encode the image
-encoded = face_recognition.face_encodings(images)[0]
+encoded = face_recognition.face_locations(images)
 
 for face_location in encoded:
-    top, right, left, bottom = face_location
+    top, right, bottom, left =  face_location
 
     face_image = images[top:bottom, left:right]
+    pil_image = Image.fromarray(face_image)
+    pil_image.show()
