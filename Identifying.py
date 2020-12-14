f4f8ed188e6781cb8404ed4989f77d443033429a
Aaris-Kazi <aariskazi@gmail.com>
2020-12-14 20:06:58 +0530

    To identify multiple pictures

--- Identifying.py
+++ Identifying.py
@@ -1,3 +1,41 @@
 from PIL import Image, ImageDraw
-import face_recognition
+import face_recognition as fr
 
+img = fr.load_image_file('./faces/bill gates.jpg')
+encoded = fr.face_encodings(img)
+
+img1 = fr.load_image_file('./faces/elon musk.jpg')
+encoded1 = fr.face_encodings(img1)
+
+img2 = fr.load_image_file('./faces/donald trump.jpg')
+encoded2 = fr.face_encodings(img2)
+
+known_faces_encoding = [
+    encoded,
+    encoded1,
+    encoded2
+]
+
+known_faces = [
+    'Bill Gates',
+    'Elon Musk',
+    'Donald Trump'
+]
+
+test_image = fr.load_image_file('./test.jpg')
+test_location = fr.face_locations(test_image)
+test_encoded = fr.face_encodings(test_image, test_location)
+
+pil_image = Image.fromarray(test_image)
+draw = ImageDraw.Draw(pil_image)
+
+for (top, right, bottom, left), face_encode in zip(test_location, test_encoded) :
+    match = fr.compare_faces(known_faces_encoding, face_encode)
+    name = "Unknown"
+
+    if True in match:
+        match_name = match.index(True)
+        name = known_faces[match_name]
+        print('Know')
+    else:
+        print('Unknown')
