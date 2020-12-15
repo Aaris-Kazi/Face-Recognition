57412b5141cee5b43e2645c1c296e3fe69cc3bd5
Aaris-Kazi <aariskazi@gmail.com>
2020-12-15 12:56:58 +0530

    A little stage changes for testing

--- Identifying.py
+++ Identifying.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageDraw
 import face_recognition as fr
+import numpy as np
 
 img = fr.load_image_file('./faces/bill gates.jpg')
 encoded = fr.face_encodings(img)
@@ -24,18 +25,28 @@ known_faces = [
 
 test_image = fr.load_image_file('./test.jpg')
 test_location = fr.face_locations(test_image)
-test_encoded = fr.face_encodings(test_image, test_location)
+test_encoded = fr.face_encodings(test_image, test_location)[0]
 
 pil_image = Image.fromarray(test_image)
 draw = ImageDraw.Draw(pil_image)
 
 for (top, right, bottom, left), face_encode in zip(test_location, test_encoded) :
-    match = fr.compare_faces(known_faces_encoding, face_encode)
+    match = fr.compare_faces( known_faces_encoding,face_encode)
     name = "Unknown"
+    distance = fr.face_distance(known_faces_encoding,face_encode)
+    best = np.argmin(distance)
 
-    if True in match:
-        match_name = match.index(True)
+    if match[best] :
+        match_name = match.index(best)
         name = known_faces[match_name]
         print('Know')
+        draw.rectangle(( (left, top), (right, bottom)), outline=(0,0,0))
+        width, height =  draw.textsize(name)
+        draw.rectangle(((left, bottom - height - 10), (right, bottom)), fill=(0,0,0),
+        outline=(0,0,0))
+        draw.text((left + 6, bottom - height - 5), name, fill= (255,255,255,255))
     else:
         print('Unknown')
+
+del draw
+pil_image.show()
